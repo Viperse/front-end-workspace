f25ad37900f3c8229038d72f700967b880c89dc0
Viperse <129846485+Viperse@users.noreply.github.com>
2023-09-13 17:13:36 +0900

    feat : react 사용하여 07 프로젝트 생성

---
+++ youtube/resources/script.js
@@ -0,0 +1,8 @@
+const asideIcon = document.querySelector('#aside-icon');
+const main = document.querySelector('main');
+
+
+asideIcon.addEventListener('click', function() {
+    // 스타일 변경!
+    main.classList.toggle('aside-change');
+});
